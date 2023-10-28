414a946fc27c0c92b0fb685d4611373f6ac0b87e
ldvhaase <37157778+ldvhaase@users.noreply.github.com>
2023-10-28 00:04:52 -0400

    Add guards & decorators

--- src/app/user/user.controller.ts
+++ src/app/user/user.controller.ts
@@ -1,14 +1,17 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
-import { JwtStrategy } from '../auth/strategy';
-import { AuthGuard } from '@nestjs/passport';
-import { Request } from 'express';
+import { Controller, Get, Patch, UseGuards } from '@nestjs/common';
+import { JwtGuard } from '../auth/guard';
+import { GetUser } from '../auth/decorator';
+import { User } from '@prisma/client';
 
+@UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
 
     @Get('me')
-    @UseGuards(AuthGuard('jwt'))
-    getMe(@Req() req: Request) {
-        return req.user
+    getMe(@GetUser() user: User) {
+        return user
     }
+
+    @Patch()
+    editUser() { }
 }
